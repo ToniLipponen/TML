2b1677dd7bfc47d20de4f0af0ad9a574cb09076a
toni <toni.lipponen@protonmail.com>
2021-10-07 23:29:02 +0300

    Removed experimental GUI implementation. This can be found in the experimental branch. Though you should be expecting a lot of problems with that.

--- include/TML/Interface/Interface.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Components/Button.h"
-#include "Components/Checkbox.h"
-#include "Container.h"
-#include "Components/Slider.h"
-#include "Components/TextInput.h"
-#include "Components/Listbox.h"
-#include "Components/Dropmenu.h"
