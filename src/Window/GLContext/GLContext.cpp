c7e74644d2069260c8dbeef0f22a56fd040612bd
Toni Lipponen <toni.lipponen@protonmail.com>
2022-05-12 00:40:58 +0300

    Create forward compatible context on macos.

--- src/Window/GLContext/GLContext.cpp
+++ src/Window/GLContext/GLContext.cpp
@@ -15,14 +15,22 @@ namespace tml
         glfwSetErrorCallback([](int, const char* m){ std::printf("[Error]: %s\n", m); });
 
         /// Set context hints.
-#if defined(TML_USE_GLES)
+
+#if defined(TML_PLATFORM_APPLE)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#else
+    #if defined(TML_USE_GLES)
         glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-#else
+    #else
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #endif
 #endif
         glfwWindowHint(GLFW_VISIBLE, 0);
 
