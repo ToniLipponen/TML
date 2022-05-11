a6862d952beac2b955562d01657e64da70bc92ed
Toni Lipponen <toni.lipponen@protonmail.com>
2022-05-12 00:43:44 +0300

    Minor fix

--- src/Window/GLContext/GLContext.cpp
+++ src/Window/GLContext/GLContext.cpp
@@ -19,7 +19,7 @@ namespace tml
 #if defined(TML_PLATFORM_APPLE)
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #else
     #if defined(TML_USE_GLES)
