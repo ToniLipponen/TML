b23a05b9260b8fd897a0ac208d114407a537f695
Toni Lipponen <toni.lipponen@protonmail.com>
2022-05-20 17:54:58 +0300

    integer-multiplication-cast-to-long fixes.

--- src/System/Image.cpp
+++ src/System/Image.cpp
@@ -42,7 +42,9 @@ namespace tml
     : m_width(image.m_width), m_height(image.m_height), m_Bpp(image.m_Bpp), m_data(new uint8_t[m_width*m_height*m_Bpp])
     {
         if(image.m_data)
-            std::memcpy(m_data, image.m_data, m_width * m_height * static_cast<size_t>(m_Bpp));
+        {
+            std::memcpy(m_data, image.m_data, static_cast<size_t>(m_width) * m_height * m_Bpp);
+        }
     }
 
     Image::Image(Image&& image) noexcept
@@ -59,7 +61,9 @@ namespace tml
     Image& Image::operator=(const Image& rhs) noexcept
     {
         if(&rhs == this)
+        {
             return *this;
+        }
 
         this->m_width = rhs.m_width;
         this->m_height = rhs.m_height;
@@ -67,8 +71,12 @@ namespace tml
 
         delete[] m_data;
         m_data = new uint8_t[m_width * m_height * m_Bpp];
+
         if(rhs.m_data)
-            std::memcpy(m_data, rhs.m_data, m_width * m_height * static_cast<size_t>(m_Bpp));
+        {
+            std::memcpy(m_data, rhs.m_data, static_cast<size_t>(m_width) * m_height * m_Bpp);
+        }
+
         return *this;
     }
 
@@ -107,7 +115,9 @@ namespace tml
         }
 
         if(m_flipOnRead)
+        {
             FlipVertically();
+        }
 
         return returnValue;
     }
@@ -120,11 +130,11 @@ namespace tml
         if((m_width * m_height) != (w * h) || m_Bpp != Bpp)
         {
             delete[] m_data;
-            m_data = new uint8_t[w * h * static_cast<size_t>(Bpp)];
+            m_data = new uint8_t[w * h * Bpp];
         }
 
         if(data)
-            memcpy(m_data, data, w * h * static_cast<size_t>(Bpp));
+            memcpy(m_data, data, static_cast<size_t>(w) * h * Bpp);
 
         if(m_flipOnRead)
             FlipVertically();
@@ -184,7 +194,7 @@ namespace tml
     bool Image::Resize(int32_t requestedWidth, int32_t requestedHeight) noexcept
     {
         if(m_data == nullptr || m_width == 0 || m_height == 0 ||
-        (requestedWidth == (unsigned)m_width && requestedHeight == (unsigned)m_height) || (requestedWidth == 0 && requestedHeight == 0))
+        (requestedWidth == m_width && requestedHeight == m_height) || (requestedWidth == 0 && requestedHeight == 0))
             return false;
 
         if(requestedWidth == 0)
@@ -205,7 +215,9 @@ namespace tml
     bool Image::FlipVertically() noexcept
     {
         if(m_data == nullptr)
+        {
             return false;
+        }
 
         const size_t rowLen = m_width * static_cast<size_t>(m_Bpp);
         const size_t height2 = m_height / 2;
@@ -238,8 +250,11 @@ namespace tml
     bool Image::LoadSvg(const String& filename, int32_t requestedWidth, int32_t requestedHeight) noexcept
     {
         std::vector<char> data;
+
         if(!File::GetBytes(filename.cpp_str(), data))
+        {
             return false;
+        }
 
         return LoadSvg(reinterpret_cast<const uint8_t *>(data.data()), static_cast<uint32_t>(data.size()), requestedWidth, requestedHeight);
     }
@@ -249,7 +264,9 @@ namespace tml
         auto document = lunasvg::Document::loadFromData(reinterpret_cast<const char*>(data), dataSize);
 
         if(!document)
+        {
             return false;
+        }
 
         m_Bpp = 4;
         m_width = requestedWidth;
@@ -261,19 +278,25 @@ namespace tml
 
     Image::ImageType Image::GetTypeFromFilename(const String &filename) noexcept
     {
-        if(filename.empty()) //!< Empty string
+        if(filename.empty())
+        {
             return Image::None;
+        }
 
-        int64_t pos;
+        int64_t pos = static_cast<int64_t>(filename.length()) - 1;
 
-        for(pos = static_cast<int64_t>(filename.length()) - 1; pos >= 0; --pos)
+        for(; pos >= 0; --pos)
         {
             if(filename.at(pos) == '.')
+            {
                 break;
+            }
         }
 
         if(pos == 0 || pos == -1)
+        {
             return Image::None;
+        }
 
         const auto len = filename.length() - pos;
         auto str = filename.substr(pos, len).cpp_str();
