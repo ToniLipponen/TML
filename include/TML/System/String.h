ac6c27a8ce1fd061c96727743e9693551207f633
Toni Lipponen <toni.lipponen@protonmail.com>
2023-04-25 20:23:55 +0300

    Remove StringToType()

--- include/TML/System/String.h
+++ include/TML/System/String.h
@@ -1,35 +1,3 @@
 #pragma once
 #include <string>
 #include "String/tinyutf8.h"
-
-namespace tml
-{
-    template<typename T>
-    [[nodiscard]] inline constexpr T StringToType(const std::string& str) noexcept
-    {
-        if constexpr(!std::is_class<T>::value)
-        {
-            if constexpr(std::is_integral<T>::value)
-            {
-                if constexpr(std::is_same<T,bool>::value)
-                {
-                    return str == "true";
-                }
-                else if constexpr(std::is_unsigned<T>::value)
-                {
-                    return std::stoull(str);
-                }
-
-                return std::stoll(str);
-            }
-            else if constexpr(std::is_floating_point<T>::value)
-            {
-                return std::stod(str);
-            }
-
-            static_assert(true, "Convertable type is not literal.");
-        }
-
-        static_assert(true, "Convertable type is not literal.");
-    }
-}
